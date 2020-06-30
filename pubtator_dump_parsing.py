f86ae3388dd94f5e4408a869c454707af9a84aca
Tilo Himmelsbach <dertilo@googlemail.com>
2020-06-30 10:27:26 +0200

    pubtator parsing

--- pubtator_dump_parsing.py
+++ pubtator_dump_parsing.py
@@ -1,7 +1,10 @@
+import os
+
 import codecs
 import re
-from typing import List
-from commons import data_io
+from typing import List, NamedTuple
+from util import data_io
+
 
 def doc_generator(file_path):
     """
@@ -30,8 +33,42 @@ def doc_generator(file_path):
 def get_stable_id(doc_id):
     return "%s::document:0:0" % doc_id
 
+def parse_annotations(content):
+    annos = []
+    for line in content[2:]:
+        anno = line.rstrip('\n').rstrip('\r').split('\t')
+        if anno[3] == 'NO ABSTRACT':
+            continue
+        else:
+
+            # Handle cases where no CID is provided...
+            if len(anno) == 5:
+                anno.append("")
+
+            _,starti,endi,mention,typpe,concept_id = anno
+            # Handle leading / trailing whitespace
+            if mention.lstrip() != mention:
+                d = len(mention) - len(mention.lstrip())
+                starti = int(starti) + d
+                mention = mention.lstrip()
+
+            if mention.rstrip() != mention:
+                d = len(mention) - len(mention.rstrip())
+                endi = int(endi) - d
+                mention = mention.rstrip()
+
+            annos.append({
+                'start': starti,
+                'end': endi,
+                'mention': mention,
+                'type': typpe,
+                'concept-id': concept_id,
+            })
+    return annos
+
 def pubtator_parser(content:List[str]):
     """
+    based on: https://github.com/HazyResearch/snorkel-biocorpus/blob/464c8905b445a73c58cfd1d51bfcb093c21bb1f1/pubtator/parsers.py#L247
         1. PMID:       PubMed abstract identifier
         2. start:      Start-char-idx of Mention
         3. end:       End-char-idx of Mention
@@ -53,35 +90,7 @@ def pubtator_parser(content:List[str]):
     # Note: some articles do not have abstracts, however they still have this line
     doc_text += ' ' + re.split(r'\|', content[1].rstrip(), maxsplit=2)[2]
 
-    # Rest of the lines are annotations
-    annos = []
-    for line in content[2:]:
-        anno = line.rstrip('\n').rstrip('\r').split('\t')
-        if anno[3] == 'NO ABSTRACT':
-            continue
-        else:
-
-            # Handle cases where no CID is provided...
-            if len(anno) == 5:
-                anno.append("")
-
-            # Handle leading / trailing whitespace
-            if anno[3].lstrip() != anno[3]:
-                d = len(anno[3]) - len(anno[3].lstrip())
-                anno[1] = int(anno[1]) + d
-                anno[3] = anno[3].lstrip()
-
-            if anno[3].rstrip() != anno[3]:
-                d = len(anno[3]) - len(anno[3].rstrip())
-                anno[2] = int(anno[2]) - d
-                anno[3] = anno[3].rstrip()
-
-            annos.append({
-                'start_end':[anno[1],anno[2]],
-                'mention':anno[3],
-                'type':anno[4],
-                'concept-id':anno[5],
-            })
+    annos = parse_annotations(content)
 
     return {'PMID':doc_id,
             'stable_id':stable_id,
@@ -90,10 +99,8 @@ def pubtator_parser(content:List[str]):
 
 
 if __name__ == '__main__':
-    from pathlib import Path
-    home = str(Path.home())
 
-    file_path = home+'/data/PubTator/bioconcepts2pubtator_offsets.sample'
+    file_path = os.environ["HOME"]+'/code/NLP/IE/pubtator/download/bioconcepts2pubtatorcentral.offset.sample'
     g = (pubtator_parser(content) for content in doc_generator(file_path))
-    data_io.write_jsons_to_file('./parsed.jsonl',g)
+    data_io.write_jsonl('./parsed.jsonl',g)
 
